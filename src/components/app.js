3ae707aed6312efb28ed47a2c3594d8e73bbba4d
Alex Jones <ajonzy@gmail.com>
2022-01-20 21:31:02 -0500

    Added dummy data for the months

--- src/components/app.js
+++ src/components/app.js
@@ -3,12 +3,22 @@ import React, { Component } from 'react';
 import Header from "./header"
 import Footer from "./footer"
 
+import dummyData from "../../static/assets/dummyData"
+
 export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monthData: dummyData.data[1]
+    }
+  }
+
   render() {
     return (
       <div className='page-wrapper'>
-        <Header month="January" />
-        <Footer year="2022" />
+        <Header month={this.state.monthData.name} />
+        <Footer year={this.state.monthData.year} />
       </div>
     );
   }
