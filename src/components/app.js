e676c23512887887a3c539847fe11cc2b2489b0f
Alex Jones <ajonzy@gmail.com>
2022-01-20 21:10:02 -0500

    Added footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Header from "./header"
+import Footer from "./footer"
 
 export default class App extends Component {
   render() {
     return (
       <div className='page-wrapper'>
         <Header month="January" />
+        <Footer year="2022" />
       </div>
     );
   }
